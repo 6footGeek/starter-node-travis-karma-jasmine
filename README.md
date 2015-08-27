90f3f858223bcc7a8089845cc0a3e2dcf48f9fdc
Andy <6footgeek@gmail.com>
2015-08-27 13:27:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# starter-node-travis-karma-jasmine
+
+This is a simple starter project for quick jasmine node karma travis testing.
---
+++ spec/main-spec.js
@@ -0,0 +1,5 @@
+describe( 'a set of tests ', function() {
+    it( "should probably test some stuff here", function( done ) {
+        expect( 1 + 1 ).toEqual( 2 );
+    } );
+} );
